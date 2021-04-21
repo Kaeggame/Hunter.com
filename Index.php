d60c06e10cad847347e959ac33e293df8b83daae
Kaeggame <66632621+Kaeggame@users.noreply.github.com>
2021-04-21 16:32:38 +0200

    Hunter Beta
    Premiere version du site

---
+++ Index.php
@@ -0,0 +1,78 @@
+<!-- Page principal -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Les liens CSS -->
+        <link rel="stylesheet" href="css/hunter.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <link rel="stylesheet" href="css/header.css">
+
+        <meta charset="UTF-8">
+        <title>Hunter.fr</title>
+    </head>
+
+    <body>
+
+        <!-- L'en-tête de la page -->
+        <header class="mainblock">
+            <ul class="mainblock">
+                <li><img class="logo" src="images/hunterlogo.png"></li>
+                <li><img class="rec" src="images/Rectangle%20130.png" alt="un 'rectangle' dégueulasse"></li>
+                <li>E</li>
+                <li><img src="images/hunterbasket.png" alt="un panier commercial"></li>
+                <li><img src="images/hunterprofil.png" alt="une icône de profil"></li>
+            </ul>
+        </header>
+
+        <!-- Le menu de navigations, qui apparaitra quand on cllique sur le bouton -->
+        <nav class="mainblock">
+            <ul class="mainblock">
+                <li>PC</li>
+                <li>PS4</li>
+                <li>Xbox One</li>
+                <li>Switch</li>
+            </ul>
+        </nav>
+
+        <!-- Le contenu principal de la page -->
+        <section class="mainblock">
+            <p class="mainblock">rien</p>
+        </section>
+
+        <!-- Le footer...juste le footer -->
+        <footer>
+            <div class="footer">
+
+                <div class="footer1">
+                    <h2>Pour mieux nous connaître</h2>
+
+                    <p>À propos de Hunter</p>
+                    <p>Carrières</p>
+                    <p>Durabilité</p>
+                </div>
+
+                <div class="footer2">
+                    <h2>Moyen de paiment sur Hunter</h2>
+
+                    <p>Cartes de paiement</p>
+                    <p>Paiement en plusieurs fois</p>
+                    <p>Chèques-cadeaux</p>
+                    <p>Rechange en ligne</p>
+                </div>
+
+                <div class="footer3">
+                    <h2>Besoin d'aide ?</h2>
+
+                    <p>Hunter et le COVID-19</p>
+                    <p>Voir ou suivre vos commandes</p>
+                    <p>Tarifs et option de livraison</p>
+                    <p>Retours et remplacements</p>
+                    <p>Application Mobile</p>
+                    <p>Centre d'aide</p>
+                </div>
+            </div>
+        </footer>
+
+    </body>
+</html>
